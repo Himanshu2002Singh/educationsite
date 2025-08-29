6c8e869096729fd7d8fb976e5493f53eedfbae97
Himanshu Singh <89336758+Himanshu2002Singh@users.noreply.github.com>
2025-08-29 17:00:04 +0530

    Update AdmissionProcess.tsx

--- src/Austrila/AdmissionProcess.tsx
+++ src/Austrila/AdmissionProcess.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Search, FileText, CheckCircle, ClipboardCheck, Globe, FileUp, Mail } from 'lucide-react';
-
+// 
 const AustraliaAdmissionProcessSection = () => {
   const steps = [
     {
